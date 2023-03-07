f0cd642221aa0b68767b965ca5546611588c0266
Tony <mutsuoara@gmail.com>
2023-03-06 18:22:13 -0800

    add header nav menu with dropdown transition

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from 'el-transition'
+
+export default class extends Controller {
+  static targets = ['openUserMenu', 'userAuthLink']
+
+  connect() {
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('modal-trigger').click();
+      });
+    //   link.addEventListener('click', () => {});
+    });
+  }
+
+  toggleDropdownMenu() {
+    toggle(document.getElementById('menu-dropdown-items'));
+  }
+}
+
