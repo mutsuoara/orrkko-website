1dfe708df345edbe6e2ac8ab94489c78aafde373
Tony <mutsuoara@gmail.com>
2023-03-07 23:42:12 -0800

    updated features to nav menu drop down and bg colors

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { enter, leave, toggle } from 'el-transition'
 
 export default class extends Controller {
-  static targets = ['openUserMenu', 'userAuthLink']
+  static targets = ['openUserMenu', 'userAuthLink', 'closeUserMenu']
 
   connect() {
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
@@ -10,14 +10,24 @@ export default class extends Controller {
     this.userAuthLinkTargets.forEach((link) => {
       link.addEventListener('click', (e) => {
         e.preventDefault();
-        document.getElementById('modal-trigger').click();
       });
-    //   link.addEventListener('click', () => {});
     });
   }
+
+    // closeUserMenu(element) {
+    //   const userMenuClicked = document.getElementById('openUserMenu').contains(element.target);
+
+
+    //   if (!userMenuClicked && element.target.id !== 'menu-dropdown-items') {
+    //     leave(document.getElementById('openUserMenu'));
+    //     leave(document.getElementById('user-menu-button'));
+    //     leave(document.getElementById('menu-dropdown-items'));
+    //   }   
+    // }
 
   toggleDropdownMenu() {
     toggle(document.getElementById('menu-dropdown-items'));
   }
+  
 }
 
