1ca92402cc8595d910a6a3919dacd224a653f047
Tony <mutsuoara@gmail.com>
2023-03-08 01:20:16 -0800

    added pages; about us, events and locations and linked from main site

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+    def aboutus
+    end
+
+    def events
+    end
+
+    def locations
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
-
+  
+  get 'aboutus', to: 'pages#aboutus'
+  get 'events', to: 'pages#events'
+  get 'locations', to: 'pages#locations'
 end
